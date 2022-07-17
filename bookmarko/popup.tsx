2612c7fe36e8d0d370409b364cffcf01f249affa
Lax Mariappan <elaxmariappan@gmail.com>
2022-07-17 22:21:40 +0530

    added tailwind

--- bookmarko/popup.tsx
+++ bookmarko/popup.tsx
@@ -1,26 +1,32 @@
 import { useState } from "react"
 
+import "./style.css"
+
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className=" p-3  w-full bg-gray-200">
+      <div className=" bg-white  p-2">
+        <h2 className="text-2xl text-indigo-600">
+          Welcome to your{" "}
+          <a href="https://www.plasmo.com" target="_blank">
+            Plasmo
+          </a>{" "}
+          Extension!
+        </h2>
+        <input
+          className="form-input bg-gray-100"
+          onChange={(e) => setData(e.target.value)}
+          value={data}
+        />
+        <a
+          href="https://docs.plasmo.com"
+          className="bg-indigo-600 text-white p-1"
+          target="_blank">
+          View Docs
+        </a>
+      </div>
     </div>
   )
 }
